750538544615279b30a78bbb1ae94f02ca94eb2b
Duturi <kimysu383@gmail.com>
2026-01-24 16:52:47 +0900

    data:시간수정

--- js/utils/date.js
+++ js/utils/date.js
@@ -1,6 +1,6 @@
 export const getGreetingMessage = () => {
   const hour = new Date().getHours();
-  if (hour >= 5 && hour < 11) return "좋은 아침이에요";
+  if (hour >= 5 && hour < 11) return "좋은 오전이에요";
   if (hour >= 11 && hour < 17) return "좋은 오후에요";
   if (hour >= 17 && hour < 22) return "좋은 저녁이에요";
   return "안녕하세요";
